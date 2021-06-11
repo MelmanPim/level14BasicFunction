218ae58bfd4da8867e5538783db7d6ede6ed4d53
MelmanPim <59539663+MelmanPim@users.noreply.github.com>
2021-06-11 16:11:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Stap 1
+
+let maakPizza = function () {
+    console.log('Rol het pizzadeeg uit');
+    console.log('Doe de saus en toppings op het deeg');
+    console.log('Doe de pizza in de oven');
+}
+
+maakPizza();
+maakPizza();
+maakPizza();
+
+// Stap 2
+
+let maakSushi = function () {
+    console.log('Kook de rijst');
+    console.log('Leg de rijst met ingrediënten op het zeewier');
+    console.log('Rol de sushi op en snij het in stukjes');
+}
+
+maakSushi();
+maakSushi();
+maakSushi();
+maakPizza();
+maakPizza();
